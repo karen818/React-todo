a13691acea240e8faeedf39f2f98bbf99aeb71de
karen818 <klc0818@outlook.com>
2016-09-11 18:55:42 -0500

    adds add todo tests

--- app/tests/components/AddTodo.test.jsx
+++ app/tests/components/AddTodo.test.jsx
@@ -11,5 +11,30 @@ describe('AddTodo', () => {
     expect(AddTodo).toExist();
   });
 
+  it('should call onAddTodo prop with valid data', () => {
+    var todoText = 'check email';
+    var spy = expect.createSpy();
+    var addTodo = TestUtils.renderIntoDocument(<AddTodo onAddTodo={spy}/>);
+    var $el = $(ReactDOM.findDOMNode(addTodo));
+
+    addTodo.refs.todoText.value = todoText;
+    TestUtils.Simulate.submit($el.find('form')[0]);
+
+    expect(spy).toHaveBeenCalledWith(todoText);
+
+  });
+
+  it('should not call onAddTodo prop when input is invalid', () => {
+    var todoText = '';
+    var spy = expect.createSpy();
+    var addTodo = TestUtils.renderIntoDocument(<AddTodo onAddTodo={spy}/>);
+    var $el = $(ReactDOM.findDOMNode(addTodo));
+
+    addTodo.refs.todoText.value = todoText;
+    TestUtils.Simulate.submit($el.find('form')[0]);
+
+    expect(spy).toNotHaveBeenCalled(todoText);
+
+  });
 
 });
