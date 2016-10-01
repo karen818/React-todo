49b32047d7cb3bfd26d481476f075c8929305d4d
karen818 <klc0818@outlook.com>
2016-09-30 22:01:30 -0500

    adds tests for actions

---
+++ app/actions/actions.jsx
@@ -0,0 +1,29 @@
+export var setSearchText = (searchText) => {
+  return {
+    type: 'SET_SEARCH_TEXT',
+    searchText
+  }
+};
+
+export var addTodo = (text) => {
+  return {
+    type: 'ADD_TODO',
+    text
+  }
+};
+
+// toggle show completedA
+// toggle todo
+
+export var toggleShowCompleted = () => {
+  return {
+    type: 'TOGGLE_SHOW_COMPLETED'
+  }
+};
+
+export var toggleTodo = (id) => {
+  return {
+    type: 'TOGGLE_TODO',
+    id
+  }
+};
